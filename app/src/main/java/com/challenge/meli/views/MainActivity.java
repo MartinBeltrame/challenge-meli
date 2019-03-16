68b8b18f377376c120b1c8bb045d6b7816ba0afb
Martín <tinchobeltrame@gmail.com>
2019-03-16 11:53:55 -0300

    Se mueve la MainActivity al paquete views

--- app/src/main/java/com/challenge/meli/views/MainActivity.java
+++ app/src/main/java/com/challenge/meli/views/MainActivity.java
@@ -1,7 +1,9 @@
-package com.challenge.meli;
+package com.challenge.meli.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.challenge.meli.R;
 
 public class MainActivity extends AppCompatActivity {
 
