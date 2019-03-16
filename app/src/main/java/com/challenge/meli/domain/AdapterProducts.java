2634646b2527b542d02cdc2ad3d1ead06e615a49
Martín <tinchobeltrame@gmail.com>
2019-03-16 12:25:53 -0300

    Se crea el adaptador de los productos

---
+++ app/src/main/java/com/challenge/meli/domain/AdapterProducts.java
@@ -0,0 +1,64 @@
+package com.challenge.meli.domain;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.challenge.meli.R;
+import com.challenge.meli.models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterProducts extends RecyclerView.Adapter<AdapterProducts.ViewHolder> {
+
+    private List<Product> products = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_product, viewGroup, false);
+        return new ViewHolder(v);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.bind(products.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        Product product;
+
+        ImageView image;
+        TextView description;
+        TextView price;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image_product);
+            description = itemView.findViewById(R.id.description_product);
+            price = itemView.findViewById(R.id.price_product);
+        }
+
+        void bind(Product product) {
+            this.product = product;
+        }
+    }
+}
+
