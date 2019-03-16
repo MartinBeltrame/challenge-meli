980692cebf7cba958e3b55821dbc338f2d626e1c
Martín <tinchobeltrame@gmail.com>
2019-03-16 12:47:20 -0300

    Se crea el ViewModel del producto

---
+++ app/src/main/java/com/challenge/meli/viewmodels/ProductViewModel.java
@@ -0,0 +1,21 @@
+package com.challenge.meli.viewmodels;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.challenge.meli.models.Product;
+
+import java.util.List;
+
+public class ProductViewModel extends AndroidViewModel {
+
+    public ProductViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public LiveData<List<Product>> getAll(String idProduct) {
+        return null;
+    }
+}
