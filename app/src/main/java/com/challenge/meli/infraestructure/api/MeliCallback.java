a53f8f809f760388ffdf855a8d18bd21317338d3
Martín <tinchobeltrame@gmail.com>
2019-03-16 14:28:34 -0300

    Se crea la callback de Retrofit

---
+++ app/src/main/java/com/challenge/meli/infraestructure/api/MeliCallback.java
@@ -0,0 +1,43 @@
+package com.challenge.meli.infraestructure.api;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+abstract class MeliCallback<T> implements Callback<T> {
+
+    private static final String LOG_TAG = MeliCallback.class.getSimpleName();
+
+    @Override
+    public void onResponse(@NonNull Call<T> call, @NonNull Response<T> response) {
+
+        if (response.isSuccessful()) {
+            Log.d(LOG_TAG, response.code() + ":" + response.raw().request().url());
+            success(response.body());
+        } else {
+            try {
+                assert response.errorBody() != null;
+                Log.e(LOG_TAG, response.code() + ":" + response.raw().request().url() + " - " + response.errorBody().string());
+            } catch (IOException e) {
+                Log.e(LOG_TAG, response.code() + ":" + response.raw().request().url());
+                e.printStackTrace();
+            }
+            error(new Exception(String.valueOf(response.errorBody())), response.code());
+        }
+    }
+
+    @Override
+    public void onFailure(@NonNull Call<T> call, @NonNull Throwable throwable) {
+        Log.e(LOG_TAG, call.request().url() + ": " + throwable.toString());
+        error(throwable, 0);
+    }
+
+    abstract void success(T respuesta);
+
+    abstract void error(Throwable throwable, int code);
+}
