30876e4461d546f9e1a407497c3abf26d6ce16c1
Martín <tinchobeltrame@gmail.com>
2019-03-16 16:03:29 -0300

    Se agrega un modelo para la respuesta de la api

---
+++ app/src/main/java/com/challenge/meli/domain/models/ResultResponse.java
@@ -0,0 +1,13 @@
+package com.challenge.meli.domain.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultResponse {
+
+    private List<Product> results = new ArrayList<>();
+
+    public List<Product> getResults() {
+        return results;
+    }
+}
