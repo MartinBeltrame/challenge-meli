4a16176c84bc37947f2b34adda6338751b269496
Martín <tinchobeltrame@gmail.com>
2019-03-18 11:53:38 -0300

    Se agrega la validacion de las expresiones regulares

--- app/src/main/java/com/challenge/meli/domain/services/SearchServices.java
+++ app/src/main/java/com/challenge/meli/domain/services/SearchServices.java
@@ -2,9 +2,11 @@ package com.challenge.meli.domain.services;
 
 public class SearchServices {
 
+    private static final String REGULAR_EXPRESSION = "^([A-Za-z0-9]+)(\\s[A-Za-z]+)*\\s?$";
+
     public boolean isValid(String nameProduct) {
         if (nameProduct != null) {
-            return !nameProduct.isEmpty();
+            return !nameProduct.isEmpty() && nameProduct.matches(REGULAR_EXPRESSION);
         } else {
             return false;
         }
