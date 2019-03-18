b7ac7bb9f1e10c33d9d85ce230337fb45a98b1b0
Martín <tinchobeltrame@gmail.com>
2019-03-18 10:05:06 -0300

    Se crea una nueva actividad para la busqueda

---
+++ app/src/main/java/com/challenge/meli/views/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.challenge.meli.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.challenge.meli.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
