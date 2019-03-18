0deb379ef495a5154c6bd3c59c733103dc781af3
Martín <tinchobeltrame@gmail.com>
2019-03-18 14:21:28 -0300

    Se agregan componentes al detalle de producto

--- app/src/main/java/com/challenge/meli/views/DetailActivity.java
+++ app/src/main/java/com/challenge/meli/views/DetailActivity.java
@@ -2,14 +2,33 @@ package com.challenge.meli.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.challenge.meli.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private ImageView thumbnail;
+    private TextView title;
+    private TextView soldQuantity;
+    private TextView price;
+    private RecyclerView recyclerPhotos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        intializeComponents();
+    }
+
+    private void intializeComponents() {
+        thumbnail = findViewById(R.id.thumbnail);
+        title = findViewById(R.id.title);
+        soldQuantity = findViewById(R.id.sold_quantity);
+        price = findViewById(R.id.price);
+        recyclerPhotos = findViewById(R.id.recycler_photos);
     }
 }
