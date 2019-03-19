542c2b73858e51de8188bf34c7d3b5f3ee458452
Martín <tinchobeltrame@gmail.com>
2019-03-19 12:05:16 -0300

    Se agrega el menu de busqueda en la main activity

--- app/src/main/java/com/challenge/meli/views/MainActivity.java
+++ app/src/main/java/com/challenge/meli/views/MainActivity.java
@@ -7,13 +7,19 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.challenge.meli.R;
 import com.challenge.meli.domain.adapters.AdapterProducts;
 import com.challenge.meli.domain.interfaces.ErrorCallback;
 import com.challenge.meli.domain.interfaces.ListenerProduct;
+import com.challenge.meli.domain.services.SearchServices;
 import com.challenge.meli.viewmodels.ProductViewModel;
 import com.challenge.meli.views.utils.Constants;
 import com.challenge.meli.views.utils.UtilServices;
@@ -24,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements ListenerProduct, 
 
     private RecyclerView recyclerProducts;
     private ProgressBar progressBar;
+    private SearchServices searchServices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,7 @@ public class MainActivity extends AppCompatActivity implements ListenerProduct, 
         setContentView(R.layout.activity_main);
 
         adapterProducts = new AdapterProducts(this);
+        searchServices = new SearchServices();
 
         progressBar = findViewById(R.id.progressBar);
         ConstraintLayout layoutNoConnection = findViewById(R.id.layout_no_connection);
@@ -58,6 +66,38 @@ public class MainActivity extends AppCompatActivity implements ListenerProduct, 
         recyclerProducts = findViewById(R.id.recycler_product);
         recyclerProducts.setLayoutManager(new LinearLayoutManager(this));
         recyclerProducts.setAdapter(adapterProducts);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        MenuItem searchViewItem = menu.findItem(R.id.app_bar_search);
+
+        SearchView searchView = (SearchView) searchViewItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String nameProduct) {
+                searchProduct(searchView, nameProduct);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    public void searchProduct(SearchView searchView, String nameProduct) {
+        boolean valid = searchServices.isValid(nameProduct);
+        if (valid) {
+            initializeViewModel(nameProduct);
+        } else {
+            Toast.makeText(this, getString(R.string.message_invalid_name), Toast.LENGTH_SHORT).show();
+        }
+        UtilServices.hideSoftKeyboard(this, searchView);
     }
 
     @Override
