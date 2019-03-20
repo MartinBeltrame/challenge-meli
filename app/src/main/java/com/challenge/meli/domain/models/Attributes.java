4766eb4d0bec5060e6b2858c85a1dab065fc227c
Martín <tinchobeltrame@gmail.com>
2019-03-20 11:37:52 -0300

    Se crea el modelo de atributo

---
+++ app/src/main/java/com/challenge/meli/domain/models/Attributes.java
@@ -0,0 +1,20 @@
+package com.challenge.meli.domain.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Attributes {
+
+    private String name;
+    @SerializedName("value_name")
+    @Expose
+    private String valueName;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValueName() {
+        return valueName;
+    }
+}
